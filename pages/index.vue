afac1c7142d91ca55a654fda2642b3a9e132f3f5
daliuspamedytis1393 <daliuspamedytis1393@gmail.com>
2023-01-03 22:58:56 +0200

    basics

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial/>
+  <Tutorial />
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  head() {
+    return {
+      script: [
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+      ],
+    };
+  },
+  name: "IndexPage",
+};
 </script>
